24a8084195fb8183bffa6fadd1d277024dfd84d1
Pawan Kumar Mishra <mishrapawankumar66@gmail.com>
2020-07-21 19:39:55 +0530

    Increased field length

--- obe/registertopics/models.py
+++ obe/registertopics/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Topic(models.Model):
 	id = models.AutoField(primary_key=True)
-	topic = models.CharField(max_length=30)
+	topic = models.CharField(max_length=200)
 	SUBJECT = (
 		('QuantumElectronics', 'Quantum Electronics'),
 		('VLSICircuitDesignandDeviceModeling', 'VLSI Circuit Design and Device Modeling'),
